98adbd4fb458bd931749b6a9cdd1111029c04702
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-03 20:25:18 +0200

    Adds new scenario

--- legion.py
+++ legion.py
@@ -1,37 +1,47 @@
 from modules.city import City, CityBuildings
 
 
-scenario_1: City = City(
+scenario: City = City(
     campaign = "Unification of Italy",
-    name = "Roma",
+    name = "Canusium",
     buildings = CityBuildings(
         buildings = {
-            "large_farm": 5,
-            "vineyard": 1,
+            "large_mine": 6,
             "basilica": 1,
-            "farmers_guild": 1,
+            "miners_guild": 1,
         },
     ),
 )
 
-scenario_1.display_results()
+scenario.display_results()
 
 
-scenario_2: City = City(
+scenario: City = City(
     campaign = "Unification of Italy",
-    name = "Roma",
+    name = "Canusium",
     buildings = CityBuildings(
         buildings = {
-            "training_ground": 1,
-            "gladiator_school": 1,
-            "stables": 1,
-            "bordello": 1,
+            "large_mine": 5,
+            "warehouse": 1,
             "basilica": 1,
-            "hospital": 1,
-            "large_fort": 1,
-            "quartermaster": 1,
+            "miners_guild": 1,
         },
     ),
 )
 
-scenario_2.display_results()
+scenario.display_results()
+
+
+scenario: City = City(
+    campaign = "Unification of Italy",
+    name = "Canusium",
+    buildings = CityBuildings(
+        buildings = {
+            "large_mine": 6,
+            "warehouse": 1,
+            "basilica": 1,
+        },
+    ),
+)
+
+scenario.display_results()
