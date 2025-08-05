61f7bfce855272702fdf80ee7e0eff01e809514f
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-05 16:42:43 +0200

    Solves minor bugs in some cities

--- legion.py
+++ legion.py
@@ -3,14 +3,46 @@ from modules.city import City, CityBuildings
 
 scenario: City = City(
     campaign = "Unification of Italy",
-    name = "Canusium",
+    name = "Clusium",
     buildings = CityBuildings(
         buildings = {
-            "large_farm": 5,
-            "vineyard": 1,
-            "village_hall": 1,
-            "farmers_guild": 1,
-            # "miners_guild": 1,
+            "city_hall": 1,
+            "basilica": 1,
+            "hidden_grove": 1,
+            "large_mine": 6,
+        },
+    ),
+)
+
+scenario.display_results()
+
+
+scenario: City = City(
+    campaign = "Unification of Italy",
+    name = "Clusium",
+    buildings = CityBuildings(
+        buildings = {
+            "city_hall": 1,
+            "basilica": 1,
+            "miners_guild": 1,
+            "large_mine": 6,
+        },
+    ),
+)
+
+scenario.display_results()
+
+
+scenario: City = City(
+    campaign = "Unification of Italy",
+    name = "Clusium",
+    buildings = CityBuildings(
+        buildings = {
+            "city_hall": 1,
+            "basilica": 1,
+            "hidden_grove": 1,
+            "miners_guild": 1,
+            "large_mine": 5,
         },
     ),
 )
