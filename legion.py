5182fa260bae73087538068aa34b769bdc5c0a48
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-05 17:01:01 +0200

    Cleans script

--- legion.py
+++ legion.py
@@ -1,7 +1,7 @@
 from modules.city import City, CityBuildings
 
 
-scenario: City = City(
+City(
     campaign = "Unification of Italy",
     name = "Clusium",
     buildings = CityBuildings(
@@ -12,12 +12,10 @@ scenario: City = City(
             "large_mine": 6,
         },
     ),
-)
-
-scenario.display_results()
+).display_results()
 
 
-scenario: City = City(
+City(
     campaign = "Unification of Italy",
     name = "Clusium",
     buildings = CityBuildings(
@@ -28,23 +26,4 @@ scenario: City = City(
             "large_mine": 6,
         },
     ),
-)
-
-scenario.display_results()
-
-
-scenario: City = City(
-    campaign = "Unification of Italy",
-    name = "Clusium",
-    buildings = CityBuildings(
-        buildings = {
-            "city_hall": 1,
-            "basilica": 1,
-            "hidden_grove": 1,
-            "miners_guild": 1,
-            "large_mine": 5,
-        },
-    ),
-)
-
-scenario.display_results()
+).display_results()
