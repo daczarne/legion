6c4c0784c82f0f3b8bd16ebffa9b4e00fbc4ade1
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-06 16:31:09 +0200

    Tests military city

--- legion.py
+++ legion.py
@@ -7,8 +7,13 @@ City(
     buildings = {
         "city_hall": 1,
         "basilica": 1,
-        "carpenters_guild": 1,
-        "large_lumber_mill": 6,
+        "hospital": 1,
+        "training_ground": 1,
+        "gladiator_school": 1,
+        "stables": 1,
+        "bordello": 1,
+        "quartermaster": 1,
+        "large_fort": 1,
     },
 ).display_results()
 
