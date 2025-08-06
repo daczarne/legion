0111b35bb5743229f6611226f37b4415541dfed0
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-06 19:23:00 +0200

    Updates examples

--- legion.py
+++ legion.py
@@ -24,10 +24,10 @@ scenario = City(
     campaign = "Unification of Italy",
     name = "Clusium",
     buildings = {
-        "city_hall": 1,
+        "town_hall": 1,
         "basilica": 1,
         "carpenters_guild": 1,
-        "large_lumber_mill": 6,
+        "large_lumber_mill": 3,
     },
 )
 
