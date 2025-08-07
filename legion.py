8158904e6003bbf9568644e0ad41565a888cdb03
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-07 15:09:36 +0200

    Adds Scenario class and changes example

--- legion.py
+++ legion.py
@@ -1,30 +1,20 @@
-from modules.city import City
+from modules.scenario import Scenario
 
 
-scenario = City(
+Scenario(
     campaign = "Unification of Italy",
-    name = "Lingones",
-    buildings = {
+    city = "Lingones",
+    buildings_a = {
         "city_hall": 1,
         "basilica": 1,
         "carpenters_guild": 1,
         "forest": 1,
         "large_lumber_mill": 5,
     },
-)
-
-scenario.display_results()
-
-
-scenario = City(
-    campaign = "Unification of Italy",
-    name = "Lingones",
-    buildings = {
+    buildings_b = {
         "city_hall": 1,
         "basilica": 1,
         "carpenters_guild": 1,
         "large_lumber_mill": 6,
     },
-)
-
-scenario.display_results()
+).display_results()
--- modules/scenario.py
+++ modules/scenario.py
@@ -1,5 +1,54 @@
-from typing import TypedDict
+from rich.align import Align
+from rich.console import Console
+from rich.layout import Layout
+from rich.panel import Panel
 
-class Scenario(TypedDict):
-    production_potentials: list[int]
-    city_buildings: dict[str, int]
+from modules.building import BuildingsCount
+from modules.city import City
+
+class Scenario:
+    def __init__(
+            self,
+            campaign: str,
+            city: str,
+            buildings_a: BuildingsCount,
+            buildings_b: BuildingsCount,
+        ) -> None:
+        self.campaign: str = campaign
+        self.city: str = city
+        self.buildings_a: BuildingsCount = buildings_a
+        self.buildings_b: BuildingsCount = buildings_b
+        
+        self.city_a: City = City(
+            campaign = self.campaign,
+            name = self.city,
+            buildings = self.buildings_a,
+        )
+        
+        self.city_b: City = City(
+            campaign = self.campaign,
+            name = self.city,
+            buildings = self.buildings_b,
+        )
+    
+    def _build_results_display(self) -> Panel:
+        layout: Layout = Layout()
+        
+        layout.split_row(
+            Layout(name = "city_a", ratio = 1),
+            Layout(name = "city_b", ratio = 1),
+        )
+        
+        layout["city_a"].update(
+            renderable = Layout(renderable = Align(renderable = self.city_a._build_results_display(), align = "center")),
+        )
+        
+        layout["city_b"].update(
+            renderable = Layout(renderable = Align(renderable = self.city_b._build_results_display(), align = "center")),
+        )
+        
+        return Panel(renderable = layout, height = 39)
+    
+    def display_results(self) -> None:
+        console: Console = Console()
+        console.print(self._build_results_display())
