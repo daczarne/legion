febdba91ca56aa1d1d52a99218ec64f25c266b91
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-07 14:46:39 +0200

    Updates test scenarios

--- legion.py
+++ legion.py
@@ -3,17 +3,13 @@ from modules.city import City
 
 scenario = City(
     campaign = "Unification of Italy",
-    name = "Clusium",
+    name = "Lingones",
     buildings = {
         "city_hall": 1,
         "basilica": 1,
-        "hospital": 1,
-        "training_ground": 1,
-        "gladiator_school": 1,
-        "stables": 1,
-        "bordello": 1,
-        "quartermaster": 1,
-        "large_fort": 1,
+        "carpenters_guild": 1,
+        "forest": 1,
+        "large_lumber_mill": 5,
     },
 )
 
@@ -22,7 +18,7 @@ scenario.display_results()
 
 scenario = City(
     campaign = "Unification of Italy",
-    name = "Clusium",
+    name = "Lingones",
     buildings = {
         "city_hall": 1,
         "basilica": 1,
