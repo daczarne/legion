621941dd2cdfc8da9e6def186c415539fb25ce9f
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-09 01:05:08 +0200

    Clean up imports

--- legion.py
+++ legion.py
@@ -1,4 +1,3 @@
-from math import prod
 from modules.scenario import Scenario
 from modules.city import City
 
@@ -18,14 +17,9 @@ scenario: Scenario = Scenario(
     },
     buildings_b = {
         "city_hall": 1,
-        # "basilica": 1,
-        # "hospital": 1,
-        # "training_ground": 1,
-        # "gladiator_school": 1,
-        # "stables": 1,
-        # "bordello": 1,
-        # "quartermaster": 1,
-        # "large_fort": 1,
+        "basilica": 1,
+        "carpenters_guild": 1,
+        "large_lumber_mill": 6,
     },
 )
 
@@ -48,23 +42,23 @@ city: City = City(
     },
 )
 
-# city.display_results(
-#     city = {
-#         "include": True,
-#     },
-#     buildings = {
-#         "include": True,
-#     },
-#     effects = {
-#         "include": True,
-#     },
-#     production = {
-#         "include": True,
-#     },
-#     storage = {
-#         "include": True,
-#     },
-#     defenses = {
-#         "include": True,
-#     },
-# )
+city.display_results(
+    city = {
+        "include": True,
+    },
+    buildings = {
+        "include": True,
+    },
+    effects = {
+        "include": True,
+    },
+    production = {
+        "include": True,
+    },
+    storage = {
+        "include": True,
+    },
+    defenses = {
+        "include": True,
+    },
+)
