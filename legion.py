63459c4f4a90293b14a123525ee80e9e560a59b1
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-11 19:45:22 +0200

    Adds logic for getting the max heigth

--- legion.py
+++ legion.py
@@ -32,28 +32,35 @@ scenario: Scenario = Scenario.from_list(
     data = [
         {
             "campaign": "Unification of Italy",
-            "name": "Hernici",
+            "name": "Roma",
             "buildings": {
                 "city_hall": 1,
                 "basilica": 1,
-                "fishing_village": 1,
-                "outcrop_mine": 1,
-                "miners_guild": 1,
-                "large_mine": 4,
+                "farmers_guild": 1,
+                "large_farm": 5,
+                "vineyard": 1,
             },
         },
         {
             "campaign": "Unification of Italy",
-            "name": "Hernici",
+            "name": "Roma",
             "buildings": {
                 "city_hall": 1,
                 "basilica": 1,
-                "fishing_village": 1,
-                "outcrop_mine": 1,
-                "large_mine": 5,
+                "hospital": 1,
+                "training_ground": 1,
+                "gladiator_school": 1,
+                "stables": 1,
+                "bordello": 1,
+                "quartermaster": 1,
+                "large_fort": 1,
             },
         },
     ]
 )
 
 scenario.display_scenario_results()
+from pprint import pprint
+for city in scenario.cities_display:
+    print(city.city.name)
+    pprint(city.configuration)
--- modules/scenario.py
+++ modules/scenario.py
@@ -6,7 +6,7 @@ from rich.layout import Layout
 
 from .building import BuildingsCount
 from .city import City
-from .display import CityDisplay
+from .display import CityDisplay, DisplayConfiguration, DisplaySectionConfiguration, DEFAULT_SECTION_COLORS
 
 
 class CityDict(TypedDict):
@@ -20,17 +20,42 @@ class Scenario:
     def __init__(
             self,
             cities: list[City],
+            configuration: DisplayConfiguration | None = None,
         ) -> None:
         self.cities: list[City] = cities
-        self.cities_display: list[CityDisplay] = [CityDisplay(city = city) for city in self.cities]
+        self._user_configuration: DisplayConfiguration = configuration or {}
+        
+        self.cities_display: list[CityDisplay] = self._build_cities_display()
+        # self.configuration: DisplayConfiguration = self._build_configuration()
     
     @classmethod
     def from_list(
         cls,
         data: list[CityDict],
+        configuration: DisplayConfiguration | None = None,
     ) -> "Scenario":
         cities: list[City] = [City(**city) for city in data]
-        return cls(cities)
+        return cls(cities, configuration)
+    
+    def _get_max_buildings_length(self) -> int:
+        building_lengths: list[int] = [len(city.buildings) + 2 for city in self.cities]
+        return max(building_lengths)
+    
+    def _build_cities_display(self) -> list[CityDisplay]:
+        cities_display: list[CityDisplay] = []
+        
+        for city in self.cities:
+            city_display: CityDisplay = CityDisplay(
+                    city = city,
+                    configuration = {
+                        "buildings": {
+                            "height": self._get_max_buildings_length(),
+                        },
+                    },
+                )
+            cities_display.append(city_display)
+        
+        return cities_display
     
     def _build_scenario_display(
             self,
