86b71653103f682342b138b6c2aea52012293754
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-11 22:38:22 +0200

    Cleans up script

--- legion.py
+++ legion.py
@@ -1,33 +1,4 @@
-from modules.city import City
-from modules.display import CityDisplay
 from modules.scenario import Scenario
-
-
-city: CityDisplay = CityDisplay(
-    city = City(
-        campaign = "Unification of Italy",
-        name = "Caudini",
-        buildings = {
-            "city_hall": 1,
-            "basilica": 1,
-            "miners_guild": 1,
-            "large_mine": 1,
-            "outcrop_mine": 1,
-            "farm": 1,
-            "lumber_mill": 1,
-            "large_fort": 1,
-            "stables": 1,
-        },
-    ),
-    configuration = {
-        "defenses": {
-            "include": True,
-        },
-    },
-)
-
-city.display_city_results()
-print()
 
 
 scenario: Scenario = Scenario.from_list(
@@ -69,10 +40,25 @@ scenario: Scenario = Scenario.from_list(
                 "large_fort": 1,
             },
         },
+        {
+            "campaign": "Unification of Italy",
+            "name": "Caudini",
+            "buildings": {
+                "city_hall": 1,
+                "basilica": 1,
+                "miners_guild": 1,
+                "large_mine": 1,
+                "outcrop_mine": 1,
+                "farm": 1,
+                "lumber_mill": 1,
+                "large_fort": 1,
+                "stables": 1,
+            },
+        },
     ],
     configuration = {
         "city": {
-            "include": False,
+            "include": True,
         },
         "defenses": {
             "include": False,
