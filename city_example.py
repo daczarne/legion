ca7eac0e308d32a5aa28bf9c98c896b056e4f7a6
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-26 20:49:24 +0200

    Adds the supply dump to the city

--- city_example.py
+++ city_example.py
@@ -15,6 +15,7 @@ city: City = City.from_buildings_count(
 )
 
 city.display_city()
+
 print()
 
 city: City = City(
@@ -31,6 +32,23 @@ city: City = City(
         Building(id = "quartermaster"),
         Building(id = "large_fort"),
     ]
+)
+
+city.display_city()
+
+print()
+
+city: City = City.from_buildings_count(
+    campaign = "Conquest of Britain",
+    name = "Anderitum",
+    buildings = {
+        "city_hall": 1,
+        "basilica": 1,
+        "supply_dump": 1,
+        "farmers_guild": 1,
+        "vineyard": 1,
+        "large_farm": 4,
+    },
 )
 
 city.display_city()
