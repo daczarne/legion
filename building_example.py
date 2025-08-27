1caeeb10e0e5e68ca4eeca3123911f8c01e752ba
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-27 17:17:22 +0200

    Adds a few more relevant examsples

--- building_example.py
+++ building_example.py
@@ -14,20 +14,37 @@ print()
 large_mine: Building = Building(id = "large_mine", workers = 0)
 large_mine.display_building()
 
-print()
-print(f"Adding 2 workers to the mine...")
-print("large_mine.add_workers(qty = 2)")
-large_mine.add_workers(qty = 2)
-large_mine.display_building()
+print(large_mine.required_building)
 
 print()
-print(f"Removing 1 worker from the mine...")
-print("large_mine.remove_workers(qty = 1)")
-large_mine.remove_workers(qty = 1)
-large_mine.display_building()
+
+stables: Building = Building(id = "stables", workers = 0)
+# stables.display_building()
+
+print(stables.required_building)
 
 print()
-print(f"Setting the number of workers to 3...")
-print("large_mine.set_workers(qty = 3)")
-large_mine.set_workers(qty = 3)
-large_mine.display_building()
+
+carpenters_guild: Building = Building(id = "carpenters_guild", workers = 0)
+# carpenters_guild.display_building()
+
+print(carpenters_guild.required_building)
+
+
+# print()
+# print(f"Adding 2 workers to the mine...")
+# print("large_mine.add_workers(qty = 2)")
+# large_mine.add_workers(qty = 2)
+# large_mine.display_building()
+
+# print()
+# print(f"Removing 1 worker from the mine...")
+# print("large_mine.remove_workers(qty = 1)")
+# large_mine.remove_workers(qty = 1)
+# large_mine.display_building()
+
+# print()
+# print(f"Setting the number of workers to 3...")
+# print("large_mine.set_workers(qty = 3)")
+# large_mine.set_workers(qty = 3)
+# large_mine.display_building()
