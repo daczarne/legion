3111b6e20174b63892f86d42ea184e8e4de69c79
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-08-30 01:24:27 +0200

    Updates module docstring

--- modules/resources.py
+++ modules/resources.py
@@ -11,10 +11,12 @@ It also includes helper classes for reading resource collection data from YAML/J
 resources as if they were dictionaries.
 
 Public API:
-- Resource (Enum): Named constants for the main resource types.
-- ResourceCollectionData (TypedDict): Helper for type hints when reading YAML/JSON. While other modules may rely on
-    this for typing, end users are not expected to interact with it directly.
+
+- Resource (Enum): Named constants for the resource types.
+- ResourceCollectionData (TypedDict): Helper for type hints when reading YAML/JSON.
 - ResourceCollection (dataclass): Stores resource counts and provides dict-like access.
+
+This classes and types are meant to be used only in other modules. End-users should have no use for them.
 """
 
 from collections.abc import Iterator
