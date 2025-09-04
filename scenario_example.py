c65e2339d54218d1dddc207c30572ca1aaef0b12
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-09-04 15:39:52 +0200

    Updates scenarios

--- scenario_example.py
+++ scenario_example.py
@@ -52,77 +52,34 @@ scenario: Scenario = Scenario.from_list(
     data = [
         {
             "campaign": "Unification of Italy",
-            "name": "Caercini",
+            "name": "Hernici",
             "buildings": {
-                "city_hall": 1,
-                "basilica": 1,
-                "miners_guild": 1,
-                "mountain_mine": 1,
+                "town_hall": 1,
+                "temple": 1,
                 "outcrop_mine": 1,
-                "large_mine": 4,
+                "fishing_village": 1,
+                "hunters_lodge": 3,
             },
         },
         {
             "campaign": "Unification of Italy",
-            "name": "Caercini",
+            "name": "Hernici",
             "buildings": {
-                "city_hall": 1,
-                "basilica": 1,
-                "mountain_mine": 1,
+                "town_hall": 1,
+                "temple": 1,
                 "outcrop_mine": 1,
-                "large_mine": 5,
-            },
-        },
-        {
-            "campaign": "Unification of Italy",
-            "name": "Caercini",
-            "buildings": {
-                "city_hall": 1,
-                "miners_guild": 1,
-                "mountain_mine": 1,
-                "outcrop_mine": 1,
-                "large_mine": 5,
-            },
-        },
-        {
-            "campaign": "Unification of Italy",
-            "name": "Caercini",
-            "buildings": {
-                "city_hall": 1,
-                "basilica": 1,
-                "miners_guild": 1,
-                "mountain_mine": 1,
-                "outcrop_mine": 1,
+                "fishing_village": 1,
                 "large_mine": 3,
-                "vineyard": 1,
             },
         },
         {
             "campaign": "Unification of Italy",
-            "name": "Caercini",
+            "name": "Hernici",
             "buildings": {
-                "city_hall": 1,
-                "basilica": 1,
-                "miners_guild": 1,
-                "mountain_mine": 1,
+                "town_hall": 1,
                 "outcrop_mine": 1,
-                "large_mine": 2,
-                "vineyard": 1,
-                "gladiator_school": 1,
-            },
-        },
-        {
-            "campaign": "Unification of Italy",
-            "name": "Caercini",
-            "buildings": {
-                "city_hall": 1,
-                "basilica": 1,
-                "miners_guild": 1,
-                "mountain_mine": 1,
-                "outcrop_mine": 1,
-                "large_mine": 1,
-                "vineyard": 1,
-                "gladiator_school": 1,
+                "fishing_village": 1,
+                "large_mine": 4,
             },
         },
     ],
@@ -138,63 +95,3 @@ scenario: Scenario = Scenario.from_list(
 
 scenario.display_scenario()
 print()
-
-scenario: Scenario = Scenario.from_list(
-    data = [
-        {
-            "campaign": "Unification of Italy",
-            "name": "Latins",
-            "buildings": {
-                "city_hall": 1,
-                "basilica": 1,
-                "carpenters_guild": 1,
-                "large_lumber_mill": 6,
-            },
-        },
-        {
-            "campaign": "Unification of Italy",
-            "name": "Latins",
-            "buildings": {
-                "city_hall": 1,
-                "basilica": 1,
-                "carpenters_guild": 1,
-                "large_lumber_mill": 5,
-                "vineyard": 1,
-            },
-        },
-        {
-            "campaign": "Unification of Italy",
-            "name": "Latins",
-            "buildings": {
-                "city_hall": 1,
-                "basilica": 1,
-                "carpenters_guild": 1,
-                "large_lumber_mill": 4,
-                "vineyard": 1,
-                "gladiator_school": 1,
-            },
-        },
-        {
-            "campaign": "Unification of Italy",
-            "name": "Latins",
-            "buildings": {
-                "city_hall": 1,
-                "basilica": 1,
-                "carpenters_guild": 1,
-                "large_lumber_mill": 3,
-                "vineyard": 1,
-                "gladiator_school": 1,
-            },
-        },
-    ],
-    configuration = {
-        "storage": {
-            "include": False,
-        },
-        "defenses": {
-            "include": False,
-        },
-    },
-)
-
-scenario.display_scenario()
