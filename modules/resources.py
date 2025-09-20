ac287ee7153638137ee1ecbee7bfb1fe014726b1
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-09-20 15:22:19 +0200

    Introduces adjustments `modules/resources.py`
    This commit adjusts the file so that they comply with the lint and
    format rules set out in Ruff.

--- modules/resources.py
+++ modules/resources.py
@@ -19,10 +19,15 @@ Public API:
 This classes and types are meant to be used only in other modules. End-users should have no use for them.
 """
 
-from collections.abc import Iterator
+from __future__ import annotations
+
 from dataclasses import dataclass, fields
 from enum import Enum
-from typing import TypedDict
+from typing import TYPE_CHECKING, TypedDict
+
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
 
 
 __all__: list[str] = []
@@ -37,6 +42,7 @@ class Resource(Enum):
         ORE: Represents ore resources.
         WOOD: Represents wood resources.
     """
+    
     FOOD = "food"
     ORE = "ore"
     WOOD = "wood"
@@ -47,6 +53,7 @@ class ResourceCollectionData(TypedDict):
     This is a helper class meant to be used when reading ResouceCollections from YAML or JSON files. Its only purpose
     is to provide good type annotations and hints.
     """
+    
     food: int
     ore: int
     wood: int
@@ -55,9 +62,9 @@ class ResourceCollectionData(TypedDict):
 @dataclass
 class ResourceCollection:
     """
-    Stores counts of resources and provides dictionary-like access.
+    Stores resource values and provides dictionary-like access.
     
-    Each instance tracks the resource: food, ore, and wood. Supports iteration and retrieval like a dictionary.
+    Each instance tracks the resources: food, ore, and wood. Supports iteration and retrieval like a dictionary.
     
     Public methods:
         __iter__(): Iterate over resource names.
@@ -66,32 +73,48 @@ class ResourceCollection:
         get(key): Get the count for a given resource name. Raises KeyError if the key is not found.
         find_fields_by_value(value): Returns a list of all the resources that have a given value.
     """
+    
     food: int = 0
     ore: int = 0
     wood: int = 0
     
     def __iter__(self) -> Iterator[str]:
-        """
-        Iterate over keys, like a dict.
-        """
         return (field.name for field in fields(class_or_instance = self))
     
     def items(self) -> Iterator[tuple[str, int]]:
         """
-        Return an iterator of (key, value) pairs, like dict.values().
+        Returns an iterator of (key, value) pairs, like `dict.items()`.
+        
+        Returns:
+            Iterator[tuple[str, int]]: An iterator of (key, value) pairs
         """
+        
         return ((field.name, getattr(self, field.name)) for field in fields(class_or_instance = self))
     
     def values(self) -> Iterator[int]:
         """
-        Return an iterator of values, like dict.values().
+        Return an iterator of values, like `dict.values()`.
+        
+        Returns:
+            Iterator[int]: An iterator of values.
         """
+        
         return (getattr(self, field.name) for field in fields(class_or_instance = self))
     
     def get(self, key: str) -> int:
         """
-        Get the value for a given resource name.
+        Get the value for a given resource.
+        
+        Args:
+            key (str): The name of a key to get.
+        
+        Raises:
+            KeyError: If the provided key does not exist.
+        
+        Returns:
+            int: The value for that key.
         """
+        
         if key not in (f.name for f in fields(class_or_instance = self)):
             raise KeyError(f"Invalid resource name: {key}")
         
@@ -100,5 +123,12 @@ class ResourceCollection:
     def find_fields_by_value(self, value: int) -> list[str]:
         """
         Return a list of resource names that have the given value.
+        
+        Args:
+            value (int): The value to look for.
+        
+        Returns:
+            list[str]: A list of resources that have that value.
         """
+        
         return [field.name for field in fields(class_or_instance = self) if getattr(self, field.name) == value]
